c3841273b1b008c7d7a3c94b631df4e9e9854189
jean unrug <jeanunrug@gmail.com>
2022-02-12 19:29:34 +0100

    update admin table

--- mysite/account/admin.py
+++ mysite/account/admin.py
@@ -7,7 +7,13 @@ from .models import Account
 class AccountAdmin(UserAdmin):
     def __init__(self, *args, **kwargs):
         new_fields = Account.get_admin_fields()
-        self.list_display += new_fields
+        self.list_display = (
+            "username",
+            "email",
+            "is_staff",
+            "is_active",
+            "is_superuser",
+        )
         self.fieldsets[1][1]["fields"] += new_fields
         return UserAdmin.__init__(self, *args, **kwargs)
 
