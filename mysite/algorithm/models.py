ba71c81d096b2436e887fdf6256ad8bd97aa83ee
jeaunrg <jeanunrug@gmail.com>
2022-04-04 22:44:59 +0200

    add references

--- mysite/algorithm/models.py
+++ mysite/algorithm/models.py
@@ -3,10 +3,9 @@ from datetime import date
 
 import pandas as pd
 from django.db import models
+from mysite.settings import MEDIA_ROOT
 
-from mysite.settings import MEDIA_ROOT, STATIC_ROOT
-
-from .utils import dfq_to_dict, dftree_to_dict
+from .utils import algo_to_dict, question_to_dict, reference_to_dict
 
 
 def upload_questions(instance, filename):
@@ -22,23 +21,35 @@ def upload_layout(instance, filename):
         os.remove(os.path.join(MEDIA_ROOT, "users", upload_url))
     return upload_url
 
+def upload_references(instance, filename):
+    upload_url = os.path.join("algorithm", str(instance.name), "references.xlsx")
+    if os.path.isfile(os.path.join(MEDIA_ROOT, "users", upload_url)):
+        os.remove(os.path.join(MEDIA_ROOT, "users", upload_url))
+    return upload_url
+
 
 class Algorithm(models.Model):
     name = models.CharField("Nom de l'algorithme", max_length=20, primary_key=True)
     layout = models.FileField(
         upload_to=upload_layout,
-        default=os.path.join(MEDIA_ROOT, "default", "algorithm", "layout.xlsx"),
+        default=os.path.join(MEDIA_ROOT, "default", "algorithm", "algo.xlsx"),
     )
     questions = models.FileField(
         upload_to=upload_questions,
         default=os.path.join(MEDIA_ROOT, "default", "algorithm", "questions.xlsx"),
+    )
+    references = models.FileField(
+        upload_to=upload_references,
+        default=os.path.join(MEDIA_ROOT, "default", "algorithm", "references.xlsx"),
     )
     last_updated = models.DateTimeField(auto_now=True)
 
     def build(self):
         if "_algo" not in self.__dict__:
             df = pd.read_excel(self.layout, header=None)
-            self._algo, self._pbar_max = dftree_to_dict(df)
+            self._algo, self._pbar_max = algo_to_dict(df)
             df = pd.read_excel(self.questions, index_col=0)
-            self._questions = dfq_to_dict(df)
-        return {"algo": self._algo, "questions": self._questions, "pbar_max": self._pbar_max}
+            self._questions = question_to_dict(df)
+            df = pd.read_excel(self.references, index_col=0, header=0)
+            self._references = reference_to_dict(df)
+        return {"algo": self._algo, "questions": self._questions, "references": self._references, "pbar_max": self._pbar_max}
--- mysite/algorithm/utils.py
+++ mysite/algorithm/utils.py
@@ -25,13 +25,13 @@ def get_algo():
     return algo, nums
 
 
-def dftree_to_dict(df, n_questions=0):
+def algo_to_dict(df, n_questions=0):
     d = {}
     indexes = np.where(df.iloc[:, 0].notnull())[0]
     for i, ind in enumerate(indexes):
         key = str(df.iloc[ind, 0])
         if key.startswith("Q:"):
-            key = key[2:] + " #" + str(n_questions)
+            key = key.split(":")[1] + " #" + str(n_questions)
             n_questions += 1
         if str(df.iloc[ind, 1]) != "nan":
             d[key] = str(df.iloc[ind, 1])
@@ -40,13 +40,29 @@ def dftree_to_dict(df, n_questions=0):
                 sub_df = df.iloc[ind + 1 :, 1:]
             else:
                 sub_df = df.iloc[ind + 1 : indexes[i + 1], 1:]
-            d[key], n_questions = dftree_to_dict(sub_df, n_questions)
+            d[key], n_questions = algo_to_dict(sub_df, n_questions)
     return d, n_questions
 
 
-def dfq_to_dict(df):
+def question_to_dict(df):
     d = defaultdict(dict)
     for ind in df.index:
         d[ind]["question"] = df.loc[ind, "question"]
         d[ind]["answers"] = list(df.loc[ind].dropna()[1:])
     return dict(d)
+
+
+def reference_to_dict(df):
+    df = df[df.index.notnull()]
+    ref_dict = dict()
+    for ref in df.index:
+        d = defaultdict(list)
+        for colname in df.columns:
+            value = df.loc[ref, colname]
+            if isinstance(value, str):
+                if colname in ["description", "pathologie"]:
+                    d[colname] = value
+                else:
+                    d["traitement"].append(value)
+        ref_dict[str(int(ref))] = dict(d)
+    return ref_dict
